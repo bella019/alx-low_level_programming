d00e017708de5a05b3717ae67d13b3a34661a343
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-12 12:31:08 -0700

    printing combinationations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- prints the numbers from 0 to 99
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
