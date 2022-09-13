c70ae0d44df91245a66d09165605e0f4baf4c833
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-13 05:38:04 -0700

    printing putchar followed by new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "putchar.h"
+/**
+ * main - prints putchar followed by a new line
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
