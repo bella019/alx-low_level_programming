2f6eb46eaecae1d441592c39f45b70381d1b6181
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-14 01:05:48 -0700

    test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lowercase ten times followed by a new line
+ * Return: 0 (success)
+ */
+void print_alphabet_x1(void)
+{
+	int n, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
+	return (0);
+}
