21e6b0089894ae18fa24edef97049c1b4616cb03
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-15 15:13:55 -0700

    prodct of two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two intergers
+ * @a: to be multplied with b
+ * @b: to be multplied with a
+ * Return: the reult of the multplication operations"
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
