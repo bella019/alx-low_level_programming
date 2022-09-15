dd3a12bb4495a9fc8ced7670a321bb78845beb54
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-15 16:31:32 -0700

    diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,16 +12,14 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 1; j < n; j++)
 			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar (' ');
+				_putchar(' ');
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
