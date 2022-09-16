26c59bc85e612c61c5043d65daea0c5ba95ab6ea
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-15 20:19:45 -0700

    fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,21 @@ int main(void)
 	char b[] = "Buzz";
 	char fb[] = "FizzBuzz";
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
+		{
 			printf("%s", b);
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s", fb);
-		else if (i % 3 == 0)
-			printf("%s", f);
-		else if (i % 5 == 0)
-			printf("%s", b);
-		else
-			printf("%d", i);
+			else if ((i % 3 == 0) && (i % 5 == 0))
+				printf("%s ", fb);
+			else if (i % 3 == 0)
+				printf("%s ", f);
+			else if (i % 5 == 0)
+				printf("%s ", b);
+			else
+				printf("%d ", i);
+		}
+		printf("\n");
+		return (0);
 	}
-	_putchar('\n');
-	return (0);
 }
