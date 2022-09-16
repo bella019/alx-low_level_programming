f50c37bdaee47b0e6200258473c1efc5b79db4b5
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-16 13:52:19 -0700

    testing numbers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints function that prints negative and positive numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
