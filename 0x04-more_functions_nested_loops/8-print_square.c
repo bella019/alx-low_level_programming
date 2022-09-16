d6bc3cca9e584d1a9c585497976fd6733c2e9165
Adebayo Goodness <oluwadamilolaadebayo019@gmail.com>
2022-09-15 19:17:24 -0700

    printing a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,23 +2,26 @@
 /**
  * print_square - prints a square followed by a new line
  * @size: size of square
+ * Return: square made of #
  */
 void print_square(int size)
 {
+	int co, ro;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
-	} else
+	}
+	else
 	{
-		int i, j;
-
-		for (i = 1; i < size; i++)
+		for (co = 1 co <= size; co++)
 		{
-			for (j = 2; j < size; j++)
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
 			{
 				_putchar('#');
 			}
-			_putchar('\n);
+			_putchar('\n');
 		}
 	}
 }
